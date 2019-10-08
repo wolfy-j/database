c7446bfd42e27a21ba3f73a86225343489831346
Wolfy-J <wolfy.jd@gmail.com>
2019-10-08 19:42:41 +0300

    - testing travis

--- tests/install-sqlsrv.sh
+++ tests/install-sqlsrv.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -ex
+sudo apt-get update
+ACCEPT_EULA=Y sudo apt-get install -qy msodbcsql17 mssql-tools unixodbc unixodbc-dev libssl1.0.0
+
 pecl install pdo_sqlsrv-5.6.0
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql.list
-sudo apt-get update
-ACCEPT_EULA=Y sudo apt-get install -qy msodbcsql17 mssql-tools unixodbc libssl1.0.0
+#sudo apt-get update
+#ACCEPT_EULA=Y sudo apt-get install -qy msodbcsql17 mssql-tools unixodbc libssl1.0.0
