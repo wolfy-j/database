dd2516ec7de77f9e4d24db34fb40c734b4c3d0e4
Wolfy-J <wolfy.jd@gmail.com>
2019-10-08 21:09:19 +0300

    - testing travis

--- tests/install-pgsql-10.sh
+++ tests/install-pgsql-10.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -ex
-
-sudo apt-get update
-sudo apt-get --yes remove postgresql\*
+# from Doctrine
+sudo service postgresql stop
+sudo apt-get remove -q 'postgresql-*'
+sudo apt-get update -q
 sudo apt-get install -q postgresql-10 postgresql-client-10
 sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 sudo service postgresql restart
