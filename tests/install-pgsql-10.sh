66c4289cf1e51e84823e986a87cf4146598342db
Wolfy-J <wolfy.jd@gmail.com>
2019-10-08 20:46:32 +0300

    - testing travis

--- tests/install-pgsql-10.sh
+++ tests/install-pgsql-10.sh
@@ -5,5 +5,4 @@ sudo apt-get update
 sudo apt-get --yes remove postgresql\*
 sudo apt-get install -q postgresql-10 postgresql-client-10
 sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
-sudo service postgresql restart 10
-sudo \conninfo
+sudo service postgresql restart
