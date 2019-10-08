015c320bd4a3f635cab52265f4f9840655286fa0
Wolfy-J <wolfy.jd@gmail.com>
2019-10-08 20:13:04 +0300

    - testing travis

--- tests/install-pgsql-10.sh
+++ tests/install-pgsql-10.sh
@@ -8,3 +8,5 @@ sudo apt-get install -q postgresql-10 postgresql-client-10
 sudo cp /etc/postgresql/{9.6,10}/main/pg_hba.conf
 sudo service postgresql restart
 sudo service postgresql status
+sudo ps -ef | grep postgres
+sudo find /tmp/ -name .s.PGSQL.5432
